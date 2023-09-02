6fb1781bb7f3b4beda6e76d8977d92ad8aea1a70
Madhav <HP@DESKTOP-B7V6ES1>
2023-09-02 11:53:10 +0530

    Created helperMethod and DB healperMethods

--- src/test/java/com/db/hooks/DBHooks.java
+++ src/test/java/com/db/hooks/DBHooks.java
@@ -6,12 +6,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 
+import com.db.utils.HelperMethods;
+
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 
 public class DBHooks {
 	public static WebDriver driver;
+	public HelperMethods hm;
 	
 	public static WebDriver getDriver() {
 		return driver;
--- src/test/java/com/db/testRunner/Runner.java
+++ src/test/java/com/db/testRunner/Runner.java
@@ -4,6 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features",
+		///JobSearchFilters.feature",
 				 glue = {"com.db.stepdefinitions","com.db.hooks"},
 				// dryRun =true,
 				 plugin = {"pretty", "html:target/test-output/report.html"}
